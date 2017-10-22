0fc5ddebfec8fa521aa6b6fb3d33467b4c351c2b
itosho <sho.ito@supership.jp>
2017-10-22 17:50:19 +0900

    add constants get method

--- lib/idol_ec_constants_ruby.rb
+++ lib/idol_ec_constants_ruby.rb
@@ -1,5 +1,15 @@
 require "idol_ec_constants_ruby/version"
+require "toml-rb"
 
 module IdolEcConstantsRuby
-  # Your code goes here...
+  def self.get_all
+    path = File.join(File.dirname(__FILE__), 'constants', 'constants.toml')
+    TomlRB.load_file(path)
+  end
+
+  def self.get(key_name)
+    path = File.join(File.dirname(__FILE__), 'constants', 'constants.toml')
+    constants = TomlRB.load_file(path)
+    constants[key_name]
+  end
 end
